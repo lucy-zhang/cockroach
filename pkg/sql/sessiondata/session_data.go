af14a58e651c747e1c8ec2fb1603739288a08ffa
Andrew Werner <ajwerner@cockroachlabs.com>
2021-01-25 18:49:03 -0500

    sql: hook up new schema change to connExecutor
    This commit adds support for execution for the statement and
    pre-commit phases of the schema changer to the `connExecutor`.
    
    Also adds a session/cluster setting
    `sql.defaults.experimental_new_schema_changer.mode`, which is `'off'`
    default. The setting takes on the following three values:
    
     * `'off'` - never use the new schema changer
     * `'on'` - use the new schema changer in supported, implicit transaction
     * `'unsafe_always'` - always try to use the new schema changer, return errors
       where not supported
    
    This code also hooks up the planning logic for the above settings. As of this
    commit, only `ALTER TABLE ... ADD COLUMN` is supported, and even then, only for
    a subset of its behaviors.
    
    Release note: None

--- pkg/sql/sessiondata/session_data.go
+++ pkg/sql/sessiondata/session_data.go
@@ -233,6 +233,9 @@ type LocalOnlySessionData struct {
 	// TODO(rytaft): remove this once unique without index constraints are fully
 	// supported.
 	EnableUniqueWithoutIndexConstraints bool
+
+	// NewSchemaChangerMode indicates whether to use the new schema changer.
+	NewSchemaChangerMode NewSchemaChangerMode
 	///////////////////////////////////////////////////////////////////////////
 	// WARNING: consider whether a session parameter you're adding needs to  //
 	// be propagated to the remote nodes. If so, that parameter should live  //
@@ -392,6 +395,53 @@ func SerialNormalizationModeFromString(val string) (_ SerialNormalizationMode, o
 		return SerialUsesVirtualSequences, true
 	case "SQL_SEQUENCE":
 		return SerialUsesSQLSequences, true
+	default:
+		return 0, false
+	}
+}
+
+// NewSchemaChangerMode controls if and when the new schema changer is in use.
+// Since 2.1, we run everything through the DistSQL infrastructure,
+// and these settings control whether to use a distributed plan, or use a plan
+// that only involves local DistSQL processors.
+type NewSchemaChangerMode int64
+
+const (
+	// UseNewSchemaChangerOff means that we never use the new schema changer (in
+	// sql/schemachanger).
+	UseNewSchemaChangerOff NewSchemaChangerMode = iota
+	// UseNewSchemaChangerOn means that we use the new schema changer for
+	// supported statements in implicit transactions, but fall back to the old
+	// schema changer otherwise.
+	UseNewSchemaChangerOn
+	// UseNewSchemaChangerUnsafeAlways means that we attempt to use the new schema
+	// changer for all statements and return errors for unsupported statements.
+	// Used for testing/development.
+	UseNewSchemaChangerUnsafeAlways
+)
+
+func (m NewSchemaChangerMode) String() string {
+	switch m {
+	case UseNewSchemaChangerOff:
+		return "off"
+	case UseNewSchemaChangerOn:
+		return "on"
+	case UseNewSchemaChangerUnsafeAlways:
+		return "unsafe_always"
+	default:
+		return fmt.Sprintf("invalid (%d)", m)
+	}
+}
+
+// NewSchemaChangerModeFromString converts a string into a NewSchemaChangerMode
+func NewSchemaChangerModeFromString(val string) (_ NewSchemaChangerMode, ok bool) {
+	switch strings.ToUpper(val) {
+	case "OFF":
+		return UseNewSchemaChangerOff, true
+	case "ON":
+		return UseNewSchemaChangerOn, true
+	case "UNSAFE_ALWAYS":
+		return UseNewSchemaChangerUnsafeAlways, true
 	default:
 		return 0, false
 	}
